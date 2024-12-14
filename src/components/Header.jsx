f2ee08bc463cadc3ae856e54e735558d2baebdc6
BBarr0c0 <115066848+BBarr0c0@users.noreply.github.com>
2024-12-14 17:52:51 +0100

    + mise en place du hearder et footer : - modification header.jsx : ajout de la navigation - ajout style pour header et footer (desktop)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,33 @@
 import '../styles/components/_header.scss';
+import { Link, NavLink } from 'react-router-dom';
+import logoHeader from '../assets/logo_header.svg';
 
 const Header = () => {
     return (
         <header className="header">
-            <h1>HEADER</h1>
+            <Link to="/">
+                <img src={logoHeader} alt="Kasa Logo" className="header-logo" />
+            </Link>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink 
+                            to="/" 
+                            className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}
+                        >
+                            Accueil
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to="/about" 
+                            className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}
+                        >
+                            À Propos
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 };
