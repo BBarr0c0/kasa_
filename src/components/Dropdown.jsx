2406d4888b5c27a33f23eff341611cb513dd9f2e
BBarr0c0 <115066848+BBarr0c0@users.noreply.github.com>
2024-12-14 19:03:06 +0100

    + mise en place de la page à propos : - ajout du composant Dropdown - modifications About - ajout du style nécessaire

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import '../styles/components/_dropdown.scss';
+
+const Dropdown = ({ title, content }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className={`dropdown`}>
+            <div className="dropdown-header" onClick={toggleDropdown}>
+                <h3>{title}</h3>
+                <span className={`arrow ${isOpen ? 'down' : 'up'}`}></span>
+            </div>
+            {isOpen && <div className="dropdown-content">{content}</div>}
+        </div>
+    );
+};
+
+Dropdown.propTypes = {
+    title: PropTypes.string,
+    content: PropTypes.string,
+};
+
+export default Dropdown;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,38 @@
 import '../styles/pages/_about.scss';
 import Banner from '../components/Banner';
 import bannerAbout from '../assets/banner_about.png';
+import Dropdown from '../components/Dropdown';
 
 
+const aboutList = [
+    {
+        title: 'Fiabilité',
+        content:
+            'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
+    },
+    {
+        title: 'Respect',
+        content:
+            'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+    },
+    {
+        title: 'Service',
+        content:
+            "La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque intéraction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.",
+    },
+    {
+        title: 'Sécurité',
+        content:
+            "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+];
 const About = () => {
     return (
         <main className="about">
-            <Banner image={bannerAbout} />
-            <h1>A PROPOS</h1>
+             <Banner image={bannerAbout} />
+             {aboutList.map((item, index) => (
+                <Dropdown key={index} title={item.title} content={item.content} />
+            ))}
         </main>
     );
 };
